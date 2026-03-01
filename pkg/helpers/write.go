588daf4a90125d3bf5ad43a145373ab05ddb7013
27dimon27 <dmnagdasev@gmail.com>
2026-03-02 00:55:10 +0300

    [fix] исправлена функция JSONResponse

--- pkg/helpers/write.go
+++ pkg/helpers/write.go
@@ -2,20 +2,16 @@ package helpers
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func JSONResponse(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
-	js, err := json.Marshal(data)
-	if err != nil {
-		// Если данные "сломаны", отдаем 500
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		slog.Error("Server failed", "error", err)
+		os.Exit(1)
 	}
-
-	w.WriteHeader(status)
-	w.Write(js)
 }
