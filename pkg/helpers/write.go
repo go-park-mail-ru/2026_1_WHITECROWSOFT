98d2ec7dcb1fed4fa2f9e6a4607a18f791fc6dbf
Andrey Nikitin <andrew@a71.su>
2026-03-11 16:19:41 +0300

    [fix] Fix code 204 JSON test

--- pkg/helpers/write.go
+++ pkg/helpers/write.go
@@ -8,6 +8,9 @@ import (
 func JSONResponse(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(status)
+	if data == nil {
+		return
+	}
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
